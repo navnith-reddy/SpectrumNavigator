aca1d4ce5307aa6ea410de06568b622d02324ce9
navnith-reddy <navnith.m.reddy@gmail.com>
2022-09-12 10:30:50 +1000

    compiled geodataframe, added results preview

--- HCIS.py
+++ HCIS.py
@@ -1,4 +1,5 @@
 import geopandas as gpd
+import pandas as pd
 import requests
 import os
 import zipfile
@@ -19,3 +20,29 @@ def getASMG():
         zip_ref.extractall("./ASMG")
     
     return
+
+def buildASMG():
+    """Returns geodataframe of ASMG for EVERY HCIS ID.
+
+    Returns:
+        asmg (geodataframe): geodataframe containing every HCIS cell.
+    """
+    
+    l1 = gpd.read_file("ASMG/ASMG_2012_GDA94_L1.shp")
+    l1.rename(columns={'HCI_Level1':'HCIS_ID'}, inplace=True)
+    l1.drop(columns=['HCI_Level2', 'HCI_Level3', 'HCI_Level4'])
+
+    l2 = gpd.read_file("ASMG/ASMG_2012_GDA94_L2.shp")
+    l2.rename(columns={'HCI_Level2':'HCIS_ID'}, inplace=True)
+    l2.drop(columns=['HCI_Level3', 'HCI_Level4'])
+
+    l3 = gpd.read_file("ASMG/ASMG_2012_GDA94_L3.shp")
+    l3.rename(columns={'HCI_Level3':'HCIS_ID'}, inplace=True)
+    l3.drop(columns=['HCI_Level4'])
+
+    l4 = gpd.read_file("ASMG/ASMG_2012_GDA94_L4.shp")
+    l4.rename(columns={'HCI_Level4':'HCIS_ID'}, inplace=True)
+
+    asmg = pd.concat([l1, l2, l3, l4])
+    
+    return asmg
