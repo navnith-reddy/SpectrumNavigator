047e52171018cb7ed3646a3a49550dbb177590a5
navnith-reddy <navnith.m.reddy@gmail.com>
2022-11-10 12:25:51 +1100

    work on GUI

---
+++ gui.py
@@ -0,0 +1,17 @@
+import customtkinter as ct
+
+def doLicExtract():
+    dialog = ct.CTkInputDialog(master=None, text="Type in a Licence number:", title="Test")
+    print("Licence Number:", dialog.get_input())
+    
+def doCliExtract():
+    dialog = ct.CTkInputDialog(master=None, text="Type in a Client ID:", title="Test")
+    print("Client ID:", dialog.get_input())
+    
+def doPoly2HCIS():
+    dialog = ct.CTkInputDialog(master=None, text="Type in a Client ID:", title="Test")
+    print("Client ID:", dialog.get_input())
+
+def doHoldings():
+    dialog = ct.CTkInputDialog(master=None, text="Type in a Client ID:", title="Test")
+    print("Client ID:", dialog.get_input())
--- spectrumNavigator.py
+++ spectrumNavigator.py
@@ -1,9 +1,11 @@
 import rrl
 import HCIS
 import os
+import gui
 import geopandas as gpd
 import pandas as pd
 import customtkinter as ct
+import tkinter as tk
 
 def clientExtract (clientIDs, data, asmg):
 
@@ -23,44 +25,33 @@ def holdingsSummary (clientNums):
     
     return
 
-# INITIALISE
-#data = rrl.readSpecData()
-#asmg = HCIS.buildASMG()
-
-import tkinter as tk
-
 ct.set_appearance_mode("light")
 ct.set_default_color_theme("green")
 
 app = ct.CTk()
-app.geometry("1200x400")
+app.geometry("600x400")
 
+#label = ct.CTkLabel(master=app, text='INITIALIZING...')
+#label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-def doLicExtract():
-    dialog = ct.CTkInputDialog(master=None, text="Type in a Licence number:", title="Test")
-    print("Licence Number:", dialog.get_input())
-    
-def doCliExtract():
-    dialog = ct.CTkInputDialog(master=None, text="Type in a Client ID:", title="Test")
-    print("Client ID:", dialog.get_input())
-    
-def doPoly2HCIS():
-    dialog = ct.CTkInputDialog(master=None, text="Type in a Client ID:", title="Test")
-    print("Client ID:", dialog.get_input())
+# INITIALISE
+#data = rrl.readSpecData()
+#asmg = HCIS.buildASMG()
 
-def doHoldings():
-    dialog = ct.CTkInputDialog(master=None, text="Type in a Client ID:", title="Test")
-    print("Client ID:", dialog.get_input())
+mainFrame = ct.CTkFrame(master=app, 
+                    width=200,
+                    height=200,
+                    corner_radius=20)
 
+licButton = ct.CTkButton(mainFrame, text="Licence Extract", command=gui.doLicExtract)
+licButton.place(relx=0.5, rely=0.25, anchor=tk.CENTER)
 
+cliButton = ct.CTkButton(mainFrame, text="Client Extract", command=gui.doCliExtract)
+cliButton.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-licButton = ct.CTkButton(app, text="Licence Extract", command=doLicExtract)
-licButton.place(relx=0.25, rely=0.5, anchor=tk.CENTER)
+hcisButton = ct.CTkButton(mainFrame, text="HCIS Conversion", command=gui.doPoly2HCIS)
+hcisButton.place(relx=0.5, rely=0.75, anchor=tk.CENTER)
 
-cliButton = ct.CTkButton(app, text="Client Extract", command=doCliExtract)
-cliButton.place(relx=0.50, rely=0.5, anchor=tk.CENTER)
-
-cliButton = ct.CTkButton(app, text="HCIS Conversion", command=doCliExtract)
-cliButton.place(relx=0.75, rely=0.5, anchor=tk.CENTER)
+mainFrame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
 app.mainloop()
