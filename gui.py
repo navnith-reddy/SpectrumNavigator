01f4d655fab330409ac9faf8262f6709e7a45052
navnith-reddy <navnith.m.reddy@gmail.com>
2022-11-14 10:18:59 +1100

    working on App class

--- gui.py
+++ gui.py
@@ -1,4 +1,25 @@
 import customtkinter as ct
+import tkinter as tk
+
+class App(ct.CTK):
+    
+    # Window dimensions as class attributes
+    WIDTH = 780
+    HEIGHT = 520
+    
+    def __init__(self):
+        
+        # super allows access to parent class members
+        super().__init__()
+        
+        self.title("Spectrum Navigator")
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        # Call .on_closing() when app gets closed
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        
+        # ---------------- CREATE TWO FRAMES -------------------
+        
+        self.grid_columnconfigure
 
 def doLicExtract():
     dialog = ct.CTkInputDialog(master=None, text="Type in a Licence number:", title="Test")
