8194264fa4a4e75a8267ae92af8bd8ba4aae7dc1
navnith-reddy <navnith.m.reddy@gmail.com>
2022-11-15 09:10:33 +1100

    working on licence extract page

--- gui.py
+++ gui.py
@@ -23,6 +23,7 @@ class App(ct.CTk):
         
         self.landingPage()
     
+    # /////////////////////// POTENTIAL MEMORY LEAK //////////////////////
     def landingPage (self):
         
         # --------------------- CREATE TWO FRAMES ------------------------
@@ -31,11 +32,11 @@ class App(ct.CTk):
         self.grid_columnconfigure(1, weight=2)
         self.grid_rowconfigure(0, weight=1)
         
-        self.panel = ct.CTkFrame(master=self, width=180, corner_radius=5)
-        self.panel.grid(row=0, column=0, sticky="nswe", padx=5, pady=10)
+        self.panel = ct.CTkFrame(master=self, width=180, corner_radius=0)
+        self.panel.grid(row=0, column=0, sticky="nswe")
         
-        self.right = ct.CTkFrame(master=self, width=360, corner_radius=5)
-        self.right.grid(row=0, column=1, sticky="nswe", padx=5, pady=10)
+        self.right = ct.CTkFrame(master=self, width=360, corner_radius=10)
+        self.right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
         
         # -------------------------- PANEL -------------------------------
         
@@ -47,10 +48,10 @@ class App(ct.CTk):
         self.panel.grid_rowconfigure(3, weight=1)
        
         # Setup Panel Prompt
-        self.prompt = ct.CTkLabel(master=self.panel,
+        prompt = ct.CTkLabel(master=self.panel,
                             text="Select operation:",
                             text_font=('Arial', -16))
-        self.prompt.grid(row=0, column=0, padx = 1, pady = 1)       
+        prompt.grid(row=0, column=0, padx = 1, pady = 1)       
        
         # Setup Panel buttons 
         licButton = ct.CTkButton(master=self.panel, 
@@ -76,44 +77,114 @@ class App(ct.CTk):
         self.right.grid_rowconfigure(1, weight=2)
         self.right.grid_rowconfigure(2, weight=1)
         
-        
-        
-        self.mainTitle =  ct.CTkLabel(master=self.right,
-                            text="Spectrum Navigator",
-                            text_font=('Arial', -42))
-        self.mainTitle.grid(row=0, column=0, padx = 1, pady = 1)
+        # Program title in right frame
+        mainTitle =  ct.CTkLabel(master=self.right,
+                                text="Spectrum Navigator",
+                                text_font=('Arial', -42))
+        mainTitle.grid(row=0, column=0, padx = 1, pady = 1)
         
         # Read logo image
-        self.logo = ImageTk.PhotoImage(Image.open('./assets/ACMA_logo.png'))
-        self.logo = ct.CTkButton(master=self.right,
+        logo = ImageTk.PhotoImage(Image.open('./assets/ACMA_logo.png'))
+        logoBtn = ct.CTkButton(master=self.right,
                                 fg_color=None,
                                 hover=False,
-                                image=self.logo, 
+                                image=logo, 
                                 text="")
-        self.logo.grid(row=1, column= 0)
+        logoBtn.grid(row=1, column= 0)
         
         # Fine text
-        self.fineText =  ct.CTkLabel(master=self.right,
+        fineText =  ct.CTkLabel(master=self.right,
                             text="ACMA Spectrum Navigator - Version 0.1      Last Updated: Nov 2022",
                             text_font=('Arial', -10))
-        self.fineText.grid(row=2, column=0, padx = 1, pady = 1, sticky='s')
+        fineText.grid(row=2, column=0, padx = 1, pady = 1, sticky='s')
         
-
-    def doLicExtract():
-        dialog = ct.CTkInputDialog(master=None, text="Type in a Licence number:", title="Test")
-        print("Licence Number:", dialog.get_input())
+    def blank (self):
+        
+        # Clear Widgets
+        self.panel.destroy()
+        self.right.destroy()
+        
+        # Recreate two frames
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=2)
+        self.grid_rowconfigure(0, weight=1)
+        
+        self.panel = ct.CTkFrame(master=self, width=180, corner_radius=0)
+        self.panel.grid(row=0, column=0, sticky="nswe")
+        
+        self.right = ct.CTkFrame(master=self, width=360, corner_radius=10)
+        self.right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
     
-    def doCliExtract():
-        dialog = ct.CTkInputDialog(master=None, text="Type in a Client ID:", title="Test")
-        print("Client ID:", dialog.get_input())
+    def back(self):
+        
+        # Clear Widgets
+        self.panel.destroy()
+        self.right.destroy()
+        
+        # Go back to landing page
+        self.landingPage()
+        
+        print("Back Button Pressed")
     
-    def doPoly2HCIS():
-        dialog = ct.CTkInputDialog(master=None, text="Type in a Client ID:", title="Test")
-        print("Client ID:", dialog.get_input())
+    # Open Licence Extract 
+    def doLicExtract(self):
+        
+        # Clear window
+        self.blank()
+        
+        # -------------------------- PANEL -------------------------------
+        
+        # Setup panel grid
+        self.panel.grid_columnconfigure(0, weight=1)
+        self.panel.grid_rowconfigure(2, weight=2)
+        self.panel.grid_rowconfigure(9, weight=1)
+        
+        # Licence Number Entry
+        LicNumPrompt = ct.CTkLabel(master=self.panel,
+                                  text="Licence Number(s):",
+                                  text_font=('Arial', -16))
+        LicNumPrompt.grid(row=1, column=0)
+        
+        LicNumEntry = ct.CTkEntry(master=self.panel,
+                                  placeholder_text="Enter a single licence number \n or comma separated licence numbers",
+                                  border_width=2,
+                                  corner_radius=10)
+        LicNumEntry.grid(row=2, column=0, sticky='nswe', padx=10, pady=10)
+        
+        LicNumEntry = ct.CTkEntry(master=self.panel,
+                                  placeholder_text="Enter a single licence number \n or comma separated licence numbers",
+                                  border_width=2,
+                                  corner_radius=10)
+        LicNumEntry.grid(row=2, column=0, sticky='nswe', padx=10, pady=10)
+        
+        # Back Button
+        backBtn = ct.CTkButton(master=self.panel, 
+                                text="Back", 
+                                command=self.back)
+        backBtn.grid(row=8, column=0, padx = 30, pady = 30, sticky='nswe')
+        
+        print("Licence Number button pressed")
+    
+    def doCliExtract(self):
+        
+        # Clear window
+        self.blank()
+        
+        print("Client ID button pressed")
+    
+    def doPoly2HCIS(self):
+        
+        # Clear window
+        self.blank()
+        
+        print("HCIS Conversion button pressed")
 
-    def doHoldings():
-        dialog = ct.CTkInputDialog(master=None, text="Type in a Client ID:", title="Test")
-        print("Client ID:", dialog.get_input())
+    def doHoldings(self):
+        
+        # Clear window
+        self.blank()
+        
+        print("Holdings button pressed")
     
 if __name__ == "__main__":
     app = App()
